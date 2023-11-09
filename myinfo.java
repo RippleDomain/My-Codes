d6336ae6256c0ae38ef6990407475ec78dce1abf
Muhammet Furkan Demir <mfurkan5555@gmail.com>
2023-11-09 13:33:46 +0300

    Add files via upload
    My first code ever.

---
+++ myinfo.java
@@ -0,0 +1,10 @@
+public class myinfo {
+
+    public static void main(String[] args) {
+        String studentFirstName = "Ripple";
+        String studentLastName = "Domain";
+        int height = 175;
+        System.out.println("Student name: " + studentLastName + ", " + studentFirstName);
+        System.out.println("Student height: " + height + "cm");
+    }    
+}
